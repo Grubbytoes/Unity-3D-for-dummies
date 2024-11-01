9bd7b778dc662266a8dc00d32a765cdba899be7d
Hugo <haaa20@sussex.ac.uk>
2024-11-01 12:12:26 +0000

    Camera is now clever...!

--- Assets/Scripts/MySmartCamera.cs
+++ Assets/Scripts/MySmartCamera.cs
@@ -4,17 +4,56 @@ using UnityEngine.InputSystem;
 
 class MySmartCamera : MonoBehaviour
 {
+    enum Action
+    {
+        STILL,
+        FOLLOW,
+        RECENTER
+    }
+
     public Transform Target;
+    public float RecenterSpeed;
 
     private Vector3 _targetOffset;
+    private Vector3 _center;
+    private Vector3 _offCenter;
+    private Action _action;
 
     void Start()
     {
+        _offCenter = Vector3.zero;
+        _center = Target.position;
         _targetOffset = this.transform.position - Target.position;
+        _action = Action.STILL;
     }
 
     void LateUpdate()
     {
-        this.transform.position = Target.position + _targetOffset;
+        // _offCenter always updates, don't worry!
+        _offCenter = Target.position - _center;
+
+        if (_offCenter.magnitude > 1)
+        {
+            Move(_offCenter - _offCenter.normalized * 1);
+            _action = Action.RECENTER;
+        }
+        else if (_action == Action.FOLLOW) _action = Action.RECENTER;
+
+        if (_action == Action.RECENTER && _offCenter.magnitude > 0.1f)
+        {
+            Vector3 toMove = _offCenter;
+            if (toMove.magnitude > RecenterSpeed) toMove = toMove.normalized * RecenterSpeed;
+            Move(toMove * Time.deltaTime);
+        }
+        else
+        {
+            _action = Action.STILL;
+        }
+    }
+
+    private void Move(Vector3 toMove, float multiplier = 1)
+    {
+        this.transform.position += toMove * multiplier;
+        _center += toMove;
     }
 }
