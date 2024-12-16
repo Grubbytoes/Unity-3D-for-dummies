2b0ae6b8f636e9f81ecb184cdd359eb851f42fb5
Hugo Mac <hugoaaabbott@gmail.com>
2024-12-16 15:40:44 +0000

    Added sounds

--- Assets/Scripts/PosterAnim.cs
+++ Assets/Scripts/PosterAnim.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PosterAnim : MonoBehaviour
 {
+    protected AudioSource audio;
+
     // Start is called before the first frame update
+    void Start() 
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     public void DropPoster() {
         var anim = GetComponent<Animator>();
         anim.SetTrigger("drop");
+        audio.Play();
     }
 }
--- Assets/Scripts/SpookyDoor.cs
+++ Assets/Scripts/SpookyDoor.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 class SpookyDoor : MonoBehaviour
 {
     protected Animator anim;
-    protected AudioSource audioSource;
+    protected AudioSource[] audioSources;
 
     void Awake() {
         anim = GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
+        audioSources = GetComponents<AudioSource>();
     }
 
+    // TODO maybe make this a bit less obviously looping?
     public void StartDoorShake() {
         anim.SetBool("shakeDoor", true);
     }
 
-    public void MakeNoise(string soundKey) {
-        // TODO
-        Debug.Log($"Sound be playing sound {soundKey} - not yet implimented");
+    public void MakeNoise(int idx) {
+        if (idx == 0)
+        {
+            audioSources[idx].Play();
+        }
+        else if (idx == 1)
+        {
+            // Adding a bit of random here, as constant roaring was a little overbearing
+            if (Random.value <= 0.4) return;
+            audioSources[idx].Play();
+        }
     }
 }
